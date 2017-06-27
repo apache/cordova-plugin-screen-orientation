10f44f3f1a7025ed90e73fe53fec7e0a0d8bcea7
sbatezat <sebastien.batezat@gmail.com>
2017-06-27 17:15:02 +0200

    Update CDVOrientation.m
    Update orientation only if needed.
    Prevent switch from UIInterfaceOrientationLandscapeLeft to UIInterfaceOrientationLandscapeRight and from UIInterfaceOrientationPortraitUpsideDown to UIInterfaceOrientationPortrait

--- src/ios/CDVOrientation.m
+++ src/ios/CDVOrientation.m
@@ -53,13 +53,18 @@
     if([vc respondsToSelector:selector]) {
         ((void (*)(CDVViewController*, SEL, NSMutableArray*))objc_msgSend)(vc,selector,result);
         
+        UIDeviceOrientation deviceOrientation = [UIDevice currentDevice].orientation;
+        
+        // Update orientation only if needed.
+        // Prevent switch from UIInterfaceOrientationLandscapeLeft to UIInterfaceOrientationLandscapeRight and
+        // from UIInterfaceOrientationPortraitUpsideDown to UIInterfaceOrientationPortrait
         if ([UIDevice currentDevice] != nil){
             NSNumber *value = nil;
-            if(orientationMask == 8 || orientationMask == 12) {
+            if(orientationMask == 8 || (orientationMask == 12 && UIDeviceOrientationIsLandscape(deviceOrientation))) {
                 value = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeRight];
             } else if (orientationMask == 4){
                 value = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeLeft];
-            } else if (orientationMask == 1 || orientationMask == 3) {
+            } else if (orientationMask == 1 || (orientationMask == 3 && UIDeviceOrientationIsPortrait(deviceOrientation))) {
                 value = [NSNumber numberWithInt:UIInterfaceOrientationPortrait];
             } else if (orientationMask == 2) {
                 value = [NSNumber numberWithInt:UIInterfaceOrientationPortraitUpsideDown];
